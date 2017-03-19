e1225b7e06d5766236cd2572efc357b7ceef9a57
tsukimi1394 <umi194@gmail.com>
2017-03-19 18:51:56 +0200

    Update js.js
    almost without bugs fully working scroll

--- lesson2/jwood/js/js.js
+++ lesson2/jwood/js/js.js
@@ -12,3 +12,33 @@ $(document).ready(function(){
         $('body,html').animate({scrollTop: top}, 1000);
     });
 });
+
+$(function(){
+(function($) {
+$.fn.blockInheart = function(speed, pause) {
+    function vis(el) {
+        var top = $(el).offset().top,
+            height = $(window).height(),
+            scroll = $(window).scrollTop();
+        return scroll > top - height && scroll < top + height
+    }
+    this.each(function(indx, el) {
+        var hide = true,
+            timer;
+        $(window).scroll(function() {
+            window.clearTimeout(timer);
+            if (!vis(el)) hide = true;
+            timer = window.setTimeout(function(event) {
+                if (vis(el) && hide) $("body, html").animate({
+                    scrollTop: $(el).offset().top
+                }, speed, function() {
+                    hide = false
+                })
+            }, pause)
+        })
+    });
+    return this
+}
+})(jQuery);
+ $(".hot").blockInheart(800,50)
+   })
