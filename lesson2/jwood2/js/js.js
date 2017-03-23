4fb2b1bf9f3931cd2c1974e49f6a5197fc86f164
tsukimi1394 <umi194@gmail.com>
2017-03-23 12:31:42 +0200

    Create js.js
    Scrolling.
    Commented mousescroll

---
+++ lesson2/jwood2/js/js.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+    $("nav").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+    $(".scroll-down").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
+
+/*$(function(){
+(function($) {
+$.fn.blockInheart = function(speed, pause) {
+    function vis(el) {
+        var top = $(el).offset().top,
+            height = $(window).height(),
+            scroll = $(window).scrollTop();
+        return scroll > top - height && scroll < top + height
+    }
+    this.each(function(indx, el) {
+        var hide = true,
+            timer;
+        $(window).scroll(function() {
+            window.clearTimeout(timer);
+            if (!vis(el)) hide = true;
+            timer = window.setTimeout(function(event) {
+                if (vis(el) && hide) $("body, html").animate({
+                    scrollTop: $(el).offset().top
+                }, speed, function() {
+                    hide = false
+                })
+            }, pause)
+        })
+    });
+    return this
+}
+})(jQuery);
+ $(".section").blockInheart(800,50)
+   })*/
